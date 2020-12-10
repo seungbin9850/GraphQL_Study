3d10bf120cafa89c3c4a980268226d80957a0999
yangseungbin <tanja1206@naver.com>
2020-12-10 11:58:26 +0900

    [DEL] hello resolver

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,20 @@
 import { ApolloServer } from "apollo-server-express";
 import * as Express from "express";
-import { buildSchema, Query, Resolver } from "type-graphql";
+import { buildSchema } from "type-graphql";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-
-@Resolver()
-class HelloResolver {
-  @Query(() => String, { nullable: true })
-  async hello() {
-    return "hello world";
-  }
-}
+import { RegisterResolver } from "./resolvers/user";
 
 const main = async () => {
   await createConnection();
-  const schema = await buildSchema({ resolvers: [HelloResolver] });
+  const schema = await buildSchema({
+    resolvers: [RegisterResolver],
+  });
   const apolloServer = new ApolloServer({ schema });
   const app = Express();
   apolloServer.applyMiddleware({ app });
   app.listen(3000, () => {
-    console.log("server start");
+    console.log("server start at http://localhost:3000/graphql");
   });
 };
 
