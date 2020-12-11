7e7820b3a912cd52e0ab428547670d4492255a1c
yangseungbin <tanja1206@naver.com>
2020-12-11 15:01:27 +0900

    [ADD] isAuth middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,13 @@
+import { MiddlewareFn } from "type-graphql/dist/interfaces/Middleware";
+import { Context } from "../dto/context";
+import { verifyAccessToken } from "../utils/jwt";
+
+export const isAuth: MiddlewareFn<Context> = async ({ context }, next) => {
+  const { authorization } = context.req.headers;
+  if (!authorization) throw new Error("not authenticated");
+  try {
+    const token = authorization.split(" ")[1];
+    const payload = await verifyAccessToken(token);
+    context["payload"] = payload as any;
+  } catch (e) {}
+};
