b2aa34820f2ca35343eff31f3f35513eda7752ea
yangseungbin <tanja1206@naver.com>
2020-12-11 15:01:02 +0900

    [ADD] verify access token

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,5 +5,9 @@ import * as path from "path";
 dotenv.config({ path: path.join(__dirname + "../.env") });
 
 export const generateAccessToken = async (id: number) => {
-  return sign({ id }, process.env.JWT_SECRET!, { expiresIn: "15m" });
+  return await sign({ id }, process.env.JWT_SECRET!, { expiresIn: "15m" });
+};
+
+export const verifyAccessToken = async (token: string) => {
+  return await verify(token, process.env.JWT_SECRET!);
 };
