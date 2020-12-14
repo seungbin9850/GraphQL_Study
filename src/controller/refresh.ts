979c6919e3317d56c4588fbf8e7e125a514f8e2b
yangseungbin <tanja1206@naver.com>
2020-12-14 13:48:31 +0900

    [ADD] token refresh controller

---
+++ src/controller/refresh.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyRefreshToken,
+} from "../utils/jwt";
+import { User } from "../entity/user";
+
+export const refreshToken = async (req: Request, res: Response) => {
+  const token = req.cookies.jid;
+  if (!token) res.status(401).json({ ok: false, accessToken: "" });
+  let payload: any = null;
+  try {
+    payload = await verifyRefreshToken(token);
+  } catch (e) {
+    res.status(401).json({ ok: false, accessToken: "" });
+  }
+  const { id, tokenVersion } = payload;
+  const user: any = await User.findOne({ where: { id } });
+  if (!user || user.tokenVersion !== tokenVersion)
+    res.status(403).json({ ok: false, accessToken: "" });
+  const newTokenVersion: any = user.tokenVersion + 1;
+  res.cookie("jid", generateRefreshToken(user.id, newTokenVersion), {
+    httpOnly: true,
+  });
+  Object.assign(user, { tokenVersion: newTokenVersion });
+  await user.save();
+  res
+    .status(200)
+    .json({ ok: true, accessToken: await generateAccessToken(user.id) });
+};
