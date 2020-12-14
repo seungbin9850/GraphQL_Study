e2c26e40e05337a05d40a16ba7bd3f7f68485c98
yangseungbin <tanja1206@naver.com>
2020-12-14 13:48:40 +0900

    [ADD] app routing

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,9 @@ import { buildSchema } from "type-graphql";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { RegisterResolver } from "./resolvers/user";
+import * as cors from "cors";
+import * as cookieParser from "cookie-parser";
+import { refreshToken } from "./controller/refresh";
 
 const main = async () => {
   await createConnection();
@@ -16,6 +19,12 @@ const main = async () => {
   });
   const app = Express();
   apolloServer.applyMiddleware({ app });
+
+  app.use(cors({ credentials: true, origin: true }));
+  app.use(cookieParser());
+
+  app.post("/refresh", refreshToken);
+
   app.listen(3000, () => {
     console.log("server start at http://localhost:3000/graphql");
   });
