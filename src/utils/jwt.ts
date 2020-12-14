8d761e599e2c33e7c545b08f1e39d272c4fb44d2
yangseungbin <tanja1206@naver.com>
2020-12-14 13:48:51 +0900

    [ADD] refresh token method

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -8,6 +8,19 @@ export const generateAccessToken = async (id: number) => {
   return await sign({ id }, process.env.JWT_SECRET!, { expiresIn: "15m" });
 };
 
+export const generateRefreshToken = async (
+  id: number,
+  tokenVersion: number
+) => {
+  return await sign({ id, tokenVersion }, process.env.REFRESH_SECRET!, {
+    expiresIn: "15d",
+  });
+};
+
 export const verifyAccessToken = async (token: string) => {
   return await verify(token, process.env.JWT_SECRET!);
+};
+
+export const verifyRefreshToken = async (token: string) => {
+  return await verify(token, process.env.REFRESH_SECRET!);
 };
