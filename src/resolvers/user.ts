8944a22ad2a93e760531038fb525733c3eec5a56
yangseungbin <tanja1206@naver.com>
2020-12-15 14:46:24 +0900

    [MODIFY] resolver name user resolver

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -16,7 +16,7 @@ import { Context } from "../dto/context";
 import { isAuth } from "../middleware/auth";
 
 @Resolver(User)
-export class RegisterResolver {
+export class UserResolver {
   @Query(() => String, { name: "helloWorld", nullable: false })
   async hello() {
     return "hello world";
