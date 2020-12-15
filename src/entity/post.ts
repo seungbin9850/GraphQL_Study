15080b8db02195399bc593c5da59ea7338362ee2
yangseungbin <tanja1206@naver.com>
2020-12-15 12:01:03 +0900

    [ADD] post entity

---
+++ src/entity/post.ts
@@ -0,0 +1,39 @@
+import { Field } from "type-graphql";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
+import { User } from "./user";
+
+@Entity()
+export class Post extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true })
+  content: string;
+
+  @Column("timestamptz")
+  @CreateDateColumn()
+  createdAt: string;
+
+  @Column("timestamptz")
+  @UpdateDateColumn()
+  updatedAt: string;
+
+  @ManyToOne(() => User, (user) => user.posts, {
+    onDelete: "CASCADE",
+  })
+  @Field(() => User)
+  @JoinColumn({ name: "writerId" })
+  user: User;
+}
